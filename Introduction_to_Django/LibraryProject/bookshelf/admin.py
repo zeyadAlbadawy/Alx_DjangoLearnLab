f2c623194e59a8223ec23ea7b3b5d85b17370415
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-07-13 13:02:51 +0300

    fix: explicit admin registration with filter and search

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -7,3 +7,6 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year' )
     list_filter=('publication_year')
     search_fields = ('title', 'author')
+
+
+admin.site.register(BookAdmin, Book)
