7096e33231e88e0f9a6a2921bf0076984e5b8561
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-07-21 00:28:03 +0300

    user creation related stuff done v4!

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
@@ -20,7 +21,6 @@ class Librarian(models.Model):
     name = models.CharField(max_length=200)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
-
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
@@ -29,14 +29,16 @@ class UserProfile(models.Model):
     ]
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
 
     def __str__(self):
-        return self.role
+        return f"{self.user.username} - {self.role}"
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
-
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,31 +12,26 @@ from django.contrib.auth.decorators import  user_passes_test
 
 # ROLES
 
-def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+def check_admin(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
-def is_librarian(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+def check_librarian(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
 
-def is_member(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+def check_member(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
-
-@login_required
-@user_passes_test(is_admin)
+@user_passes_test(check_admin)
 def admin_view(request):
     return render(request, 'admin_view.html')
 
-@login_required
-@user_passes_test(is_librarian)
+@user_passes_test(check_librarian)
 def librarian_view(request):
     return render(request, 'librarian_view.html')
 
-@login_required
-@user_passes_test(is_member)
+@user_passes_test(check_member)
 def member_view(request):
     return render(request, 'member_view.html')
-
 
 def user_login(request):
     if request.method == 'POST':
