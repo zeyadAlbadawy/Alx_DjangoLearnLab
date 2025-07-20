08e6bebb5835505eca849df3404d6a28ebe0aa91
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-07-20 23:03:30 +0300

    user authentication done v1!

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def user_login(request):
