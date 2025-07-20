24d08b198a966b5eba6d7cb4c89522a158649fdb
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-07-20 18:44:40 +0300

    fucntion based views finished big fixed

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from django.views.generic import DetailView
  
 # Create your views here.
 def List_books(request):
-    books = Book.objects.select_related('author').all()
+    books = Book.objects.all()
     context = {'books': books}
     return render(request, 'relationship_app/list_books.html', context)
 
