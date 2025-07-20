0e8d49d46a79488bb4b5d68cf05a8249c52e7714
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-07-20 17:31:32 +0300

    small bug fixed!

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
