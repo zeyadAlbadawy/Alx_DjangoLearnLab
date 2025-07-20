0ef37c50136ed9e671324d2120eef362d2b795ad
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-07-20 23:02:30 +0300

    user authentication done!

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,5 +4,8 @@ from .views import list_books
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>', views.LibraryDetailView.as_view(), name = 'library_detail')
+    path('library/<int:pk>', views.LibraryDetailView.as_view(), name = 'library_detail'),
+    path('login,', views.user_login, name = 'login_user'),
+    path('logout_user/', views.user_logout, name='logout_user'),
+    path('register/', views.register, name = 'register_user')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
- 
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if(form.is_valid):
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+        context = {'from': form}
+        return render(request, 'relationship_app/login.html', context) 
+
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if(form.is_valid):
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+        context =  {'form': form}
+        return render(request, 'relationship_app/register.html', context)
+
+
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
