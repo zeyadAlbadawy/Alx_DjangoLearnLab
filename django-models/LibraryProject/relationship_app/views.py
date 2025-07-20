e961f3ba5a97ac0d18708fcf7816909ac7174cd2
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-07-20 19:19:54 +0300

    second task done -v-1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Book, Library
+from .models import Book
+from .models import Library
 from django.views.generic import DetailView
  
 # Create your views here.
