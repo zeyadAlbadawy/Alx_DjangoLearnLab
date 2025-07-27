089478e36701600e50d4d5dd04954d847e810296
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-07-27 19:22:29 +0300

    task 2 initial commit 00

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -9,7 +9,14 @@ class Book(models.Model):
     author=models.CharField(max_length=100)
     publication_year=models.IntegerField()
 
-
+class Meta:
+    permission = [
+        ('can_view', 'Can view book'),
+        ('can_create', 'Can create book'),
+        ('can_edit', 'Can edit book'),
+        ('can_delete', 'Can delete book'),
+    ]
+    
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
