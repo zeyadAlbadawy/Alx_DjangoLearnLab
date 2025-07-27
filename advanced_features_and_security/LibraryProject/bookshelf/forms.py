10ae33e56ffae63c4a4fa594be5326c3a5d7a9d9
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-07-27 19:58:11 +0300

    task 3 commit 05

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -7,14 +7,6 @@ class BookForm(forms.ModelForm):
         model = Book
         fields = ['title', 'author', 'published_date']
 
-class BookSearchForms(forms.Form):
-    query = forms.CharField(
-        max_length= 100
-        required=False
-        label='Search For A book'
-        widget=forms.TextInput(attrs={'placeholder': 'Enter Title'})
-    )
-
     class ExampleForm(forms.Form):
         name = forms.CharField(max_length=100, required=True)
         email = forms.EmailField(required=True)
