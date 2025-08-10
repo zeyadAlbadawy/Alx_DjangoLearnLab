a884801a84925771931af093a34030b08c6a2932
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-08-10 09:25:19 +0300

    task two for views handling  it finished v2

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import BookListCreateView, UpdateAndDeleteBookListView
+from .views import BookListCreateView, BookDetailView, BookUpdateView, BookDeleteView
+
 urlpatterns = [
-    path('books/', BookListCreateView.as_view, name='book-list'),
-    path('books/<int:pk>/', BookListCreateView.as_view, name='book-one')
+    path('books/', BookListCreateView.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -24,23 +24,23 @@ class BookDeleteView(generics.DestroyAPIView):
     serializer_class = BookSerializer
 
 
-class BookListCreateView(generics.CreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class=BookSerializer
+# class BookListCreateView(generics.CreateAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class=BookSerializer
 
-    # all the users can view the books but authenticated only can create
-    def get_permissions(self):
-        if self.req.method == 'GET':
-            return [permissions.AllowAny()]
-        return [permissions.IsAuthenticated()]
+#     # all the users can view the books but authenticated only can create
+#     def get_permissions(self):
+#         if self.req.method == 'GET':
+#             return [permissions.AllowAny()]
+#         return [permissions.IsAuthenticated()]
     
 
-class UpdateAndDeleteBookListView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class= BookSerializer
+# class UpdateAndDeleteBookListView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class= BookSerializer
 
-    def get_permissions(self):
-        if self.req.method == 'GET':
-            return [permissions.AllowAny()]
-        return [permissions.IsAuthenticated()]
+#     def get_permissions(self):
+#         if self.req.method == 'GET':
+#             return [permissions.AllowAny()]
+#         return [permissions.IsAuthenticated()]
 
