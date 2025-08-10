006824ea0e954969be55f75a56b6f06ca7de168d
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-08-10 09:31:07 +0300

    task two for views handling  it finished v5

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,12 +2,22 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .serializers import BookSerializer
 from .models import Book, Author
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
-class BookListView(generics.ListCreateAPIView):
+class BookCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    def get_permissions(self):
+        if self.req.method == 'GET':
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
 # Retrieve
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
@@ -18,10 +28,24 @@ class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    def get_permissions(self):
+        if self.req.method == 'GET':
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+
+
 # Delete
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.req.method == 'GET':
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+
 
 
 # class BookListCreateView(generics.CreateAPIView):
