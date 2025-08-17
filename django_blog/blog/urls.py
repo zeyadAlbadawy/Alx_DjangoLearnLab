3c470b9c4eb4f6674139f8ff9950ce1bc2fef628
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-08-17 23:05:24 +0300

    task 5 done

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
 
     path('search/', views.search_posts, name='search_posts'),
     path('tags/<str:tag_name>/', views.posts_by_tag, name='posts_by_tag'),
+    path("tags/<slug:tag_slug>/", views.PostByTagListView.as_view(), name="posts_by_tag"),
 
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -140,3 +140,18 @@ def posts_by_tag(request, tag_name):
     tag = get_object_or_404(Tag, name=tag_name)
     posts = tag.posts.all()
     return render(request, 'blog/posts_by_tag.html', {'tag': tag, 'posts': posts})
+
+
+class PostByTagListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        tag_slug = self.kwargs.get("tag_slug")
+        return Post.objects.filter(tags__slug=tag_slug).distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tag"] = self.kwargs.get("tag_slug")
+        return context
