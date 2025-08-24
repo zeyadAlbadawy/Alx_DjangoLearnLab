df8a6da56b4244d54f729c6ae9df408eb905a279
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-08-24 21:50:56 +0300

    data base config

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -85,11 +85,16 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", "5432"),  # <-- PORT explicitly included
     }
 }
+
 
 
 # Password validation
@@ -158,3 +163,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
