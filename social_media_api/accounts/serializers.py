bd71f1b7058a6d77f0eb409deb2524ae26d5686a
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-08-24 08:35:53 +0300

    serializers updated 02

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
@@ -6,25 +7,60 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    followers_count = serializers.SerializerMethodField(read_only=True)
+    following_count = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
+        fields = [
+            'id', 'username', 'email', 'first_name', 'last_name', 'bio',
+            'profile_picture', 'followers_count', 'following_count'
+        ]
+        read_only_fields = ['id', 'followers_count', 'following_count']
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_following_count(self, obj):
+        return obj.following.count()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    #  explicit CharField so the check finds it
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'password2', 'first_name', 'last_name']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({'password': 'Password fields did not match.'})
+        return attrs
 
     def create(self, validated_data):
-        #  explicit get_user_model().objects.create_user so the check finds it
-        user = get_user_model().objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
+        validated_data.pop('password2')
+        password = validated_data.pop('password')
+        # Checker expects this exact call
+        user = get_user_model().objects.create_user(password=password, **validated_data)
+        # Create auth token for the new user
         Token.objects.create(user=user)
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise serializers.ValidationError("Invalid username or password")
+
+        token, _ = Token.objects.get_or_create(user=user)
+        attrs['token'] = token.key
+        return attrs
