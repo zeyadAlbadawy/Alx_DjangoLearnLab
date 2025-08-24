846b10405ef6314102ce7a448cadabb2d2f2148b
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-08-24 09:42:56 +0300

    end points added for follower

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
-from .views import RegisterView, LoginView, ProfileView
+from .views import (
+    RegisterView,
+    LoginView,
+    ProfileView,
+    FollowUserView,
+    UnfollowUserView,
+    FollowersListView,
+    FollowingListView,
+)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('follow/<int:user_id>', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
+    path('followers/<int:user_id>/', FollowersListView.as_view(), name='followers-list'),
+    path('following/<int:user_id>/', FollowingListView.as_view(), name='following-list'),
 ]
